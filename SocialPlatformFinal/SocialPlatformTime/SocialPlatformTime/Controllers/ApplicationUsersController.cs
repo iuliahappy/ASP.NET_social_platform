38e9a33d7fb8da01a4dffe6b286b78d082230672
nailedion <adelin02baca.ionutaby@gmail.com>
2025-12-15 14:16:06 +0200

    Fixed the context in user

--- SocialPlatformFinal/SocialPlatformTime/SocialPlatformTime/Controllers/ApplicationUsersController.cs
+++ SocialPlatformFinal/SocialPlatformTime/SocialPlatformTime/Controllers/ApplicationUsersController.cs
@@ -1,72 +1,72 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using SocialPlatformTime.Data;
-//using SocialPlatformTime.Models;
-//using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialPlatformTime.Data;
+using SocialPlatformTime.Models;
+using System;
 
-//namespace SocialPlatformTime.Controllers
-//{
-//    public class ApplicationUsersController : Controller
-//    {
-//        private readonly AppDbContext _db;
+namespace SocialPlatformTime.Controllers
+{
+    public class ApplicationUsersController : Controller
+    {
+        private readonly ApplicationDbContext _db;
 
-//        public ApplicationUsersController(AppDbContext context)
-//        {
-//            _db = context;
-//        }
+        public ApplicationUsersController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
 
-//        // Show all users
-//        public IActionResult Index()
-//        {
-//            var users = _db.Users.ToList();
-//            ViewBag.Users = users;
+        // Show all users
+        public IActionResult Index()
+        {
+            var users = _db.Users.ToList();
+            ViewBag.Users = users;
 
-//            return View();
-//        }
+            return View();
+        }
 
-//        // Show user profile by id 
-//        public IActionResult Show(string id)
-//        {
-//            var user = _db.Users.Find(id);
+        // Show user profile by id 
+        public IActionResult Show(string id)
+        {
+            var user = _db.Users.Find(id);
 
-//            if (user == null)
-//            {
-//                return NotFound();
-//            }
+            if (user == null)
+            {
+                return NotFound();
+            }
 
-//            ViewBag.User = user;
-//            return View();
-//        }
+            ViewBag.User = user;
+            return View();
+        }
 
-//        // Edit user (view)
-//        public IActionResult Edit(string id)
-//        {
-//            var user = _db.Users.Find(id);
+        // Edit user (view)
+        public IActionResult Edit(string id)
+        {
+            var user = _db.Users.Find(id);
 
-//            if (user == null)
-//            {
-//                return NotFound();
-//            }
+            if (user == null)
+            {
+                return NotFound();
+            }
 
-//            return View(user);
-//        }
+            return View(user);
+        }
 
-//        // Edit user (post request)
-//        [HttpPost]
-//        public IActionResult Edit(string id, ApplicationUser updatedUser)
-//        {
-//            var user = _db.Users.Find(id);
+        // Edit user (post request)
+        [HttpPost]
+        public IActionResult Edit(string id, ApplicationUser updatedUser)
+        {
+            var user = _db.Users.Find(id);
 
-//            if (ModelState.IsValid)
-//            {
-//                user.FirstName = updatedUser.FirstName;
-//                user.LastName = updatedUser.LastName;
-//                user.PhoneNumber = updatedUser.PhoneNumber;
+            if (ModelState.IsValid)
+            {
+                user.FirstName = updatedUser.FirstName;
+                user.LastName = updatedUser.LastName;
+                user.PhoneNumber = updatedUser.PhoneNumber;
 
-//                _db.SaveChanges();
-//                return Redirect("/ApplicationUsers/Show/" + user.Id);
-//            }
+                _db.SaveChanges();
+                return Redirect("/ApplicationUsers/Show/" + user.Id);
+            }
 
-//            return View(updatedUser);
-//        }
-//    }
-//}
+            return View(updatedUser);
+        }
+    }
+}
