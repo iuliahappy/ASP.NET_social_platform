9ef9972ca3ecd246aa534837707dc94831c963a1
nailedion <adelin02baca.ionutaby@gmail.com>
2026-01-03 15:47:42 +0100

    Started the groups

---
+++ SocialPlatformFinal/SocialPlatformTime/SocialPlatformTime/Controllers/GroupsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialPlatformTime.Data;
+using SocialPlatformTime.Models;
+
+namespace SocialPlatformTime.Controllers
+{
+    public class GroupsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager) : Controller
+    {
+        private readonly ApplicationDbContext _db = context;
+        private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult New(string groupName)
+        {
+            if (string.IsNullOrWhiteSpace(groupName))
+            {
+                return RedirectToAction("Index", "Conversations");
+            }
+
+            var currentUserId = _userManager.GetUserId(User);
+
+            var newGroup = new Group
+            {
+                Name = groupName,
+                Description = "",
+                CreateDGroup = DateTime.Now
+            };
+
+            _db.Groups.Add(newGroup);
+            _db.SaveChanges();
+
+            var groupRole = new RoleTable
+            {
+                ApplicationUserId = currentUserId,
+                RoleName = "Owner",
+                GroupId = newGroup.Id
+            };
+            _db.GroupRoles.Add(groupRole);
+
+            var groupConversation = new Conversation
+            {
+                GroupId = newGroup.Id
+            };
+            _db.Conversations.Add(groupConversation);
+
+            _db.SaveChanges();
+
+            return RedirectToAction("Show", "Conversations", new { id = groupConversation.Id });
+        }
+    }
+}
